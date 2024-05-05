ac7b0f417cf83c624cb1cadaae3a1ee80b145883
Arjun Singh <arjun2314singh@gmail.com>
2024-05-06 02:28:24 +0530

    Update 01_Variables.js

--- .devcontainer/JSBasic/01_Variables.js
+++ .devcontainer/JSBasic/01_Variables.js
@@ -2,10 +2,13 @@ const customerID=12345;
 let customerName="Arjun"
 var customerPassword="arjun12"
 
+let accountState;
+
 customerCity="Hyd"
 
 // customerID=2; we cannot change value of const variable
 console.log(customerID)
 
-console.table([customerID,customerName ,customerPassword,customerCity]) 
+console.table([customerID,customerName ,customerPassword,customerCity,accountState]) 
 // here we are using console.table for printing multiple value in console
+// we should not use var we face scope problem
