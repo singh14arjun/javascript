9423ddb67855b3ac7ab2d6a8717fadbc1a465615
Arjun Singh <arjun2314singh@gmail.com>
2024-05-06 02:15:20 +0530

    Update 01_Variables.js

--- .devcontainer/JSBasic/01_Variables.js
+++ .devcontainer/JSBasic/01_Variables.js
@@ -1,1 +1,11 @@
+const customerID=12345;
+let customerName="Arjun"
+var customerPassword="arjun12"
 
+customerCity="Hyd"
+
+// customerID=2; we cannot change value of const variable
+console.log(customerID)
+
+console.table([customerID,customerName ,customerPassword,customerCity]) 
+// here we are using console.table for printing multiple value in console
